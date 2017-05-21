f41253ed44f19e0297cb242081e4ac424c90f001
pwsfans <priaw3839@gmail.com>
2017-05-21 12:15:58 +0700

    SMKInovasi

---
+++ app/src/main/java/pria/com/smkinovasi/PrefManager.java
@@ -0,0 +1,32 @@
+package pria.com.smkinovasi;
+
+/**
+ * Created by pwsfans on 26/04/2017.
+ */
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class PrefManager {
+    private static final String PREF_NAME = "androidhive-welcome";
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+    int PRIVATE_MODE = 0;
+
+    public PrefManager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+}
